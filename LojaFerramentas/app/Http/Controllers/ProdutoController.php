f11c67dcddc63f32f587348da96c7c7e944489df
iTzxEduardo <edu-100santos2011@hotmail.com>
2024-08-14 21:38:24 -0300

    14/08

---
+++ LojaFerramentas/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Produto;
+
+class ProdutoController extends Controller
+{
+    //lista todos os produtos
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos.index',compact('produtos'));
+
+    }
+
+    //abre o formulario de cadastro
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    // envia o formulario de cadastro
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome'=> 'required|string|max:255',
+            'descricao'=> 'required',
+            'categoria'=> 'required',
+            'quantidade'=> 'required|numeric',
+            'preco'=> 'required|numeric',
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')->
+        with('success','Produto Criado com Sucesso');
+    }
+
+    public function edit(Produto $produto)
+    {
+        return view('produtos.edit',compact('produto'));
+    }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome'=> 'required|string|max:255',
+            'descricao'=> 'required',
+            'categoria'=> 'required',
+            'quantidade'=> 'required|numeric',
+            'preco'=> 'required|numeric',
+        ]);
+
+        $produto->update($request->all());
+
+        return redirect()->route('produtos.index')->
+        with('success','Produto Atualizado com Sucesso');
+    }
+
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+
+        return redirect()->route('produtos.index')->
+        with('success','Produto Deletado com Sucesso');
+    }
+}
---
+++ LojaFerramentas/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\http\Controllers\ProdutoController;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/registro', [UserController::class, 'showRegistroForm'])->name('usuarios.registro');
+Route::post('/registro', [UserController::class, 'registro'])->name('usuarios.registro');
+
+
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('usuarios.login');
+Route::post('/login', [UserController::class, 'login'])->name('usuarios.login');
+
+//rota para pagina interna
+Route::get('/dashboard',function(){
+    return view('usuarios.dashboard');
+})->middleware('auth')->name('usuarios.dashboard');
+
+Route::post('/logout', [UserController::class, 'logout']);
+
+//rota para produtos
+Route::resource('produtos',ProdutoController::class);
